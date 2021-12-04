adc37e5f39a7ac84811ccaced9deec0e37ff06c3
Joao Melo <jvictorcostamelo@gmail.com>
2021-12-04 13:00:00 -0300

    hotfixsow

--- HeapMemory.py
+++ HeapMemory.py
@@ -4,10 +4,11 @@ from HeapNode import HeapNode
 class HeapMemory:
     size = 30
     heapArray = [False] * size
-    heapArrayValues  =  ['*'] * size
+    heapArrayValues  =  [''] * size
     freeSpaceList = LinkedList(size)
     node=None
     index ={}
+    indexQtd ={}
     method =''
     commands=''
     def __init__(self, commands):
@@ -26,31 +27,40 @@ class HeapMemory:
                 elif(params[0] == 'del'):
                     if(self.hasDuplicate( self.index[params[1]])):
                         position = self.index[params[1]]
-                        self.index.pop(list(self.index.keys())[list(self.index.values()).index(position)])
+                        keysList = list(self.index.keys())
+                        valuesList = list(self.index.values())
+                        self.index.pop(keysList[valuesList.index(position)])
+                        self.indexQtd.pop(keysList[valuesList.index(position)])
+                 
                     self.removeBlock(self.index[params[1]])
+                    self.index.pop(params[1])
+                    self.indexQtd.pop(params[1])
+
                 elif(params[0] == 'new'):
                     if(self.method=='wrost'):
                         self.index[params[1]] = self.worstFit(int(params[2]))
+                        self.indexQtd[params[1]] =  int(params[2])
                     if(self.method == 'best'):
                         self.index[params[1]] = self.bestFit(int(params[2]))
+                        self.indexQtd[params[1]] = int(params[2])
                     self.setHeap(self.index[params[1]],int(params[2]),params[1])
                 elif(params[1]== '='):
                     self.index[params[0]] = int(self.index[params[2]])
-
+                    self.indexQtd[params[0]] = int(self.indexQtd[params[2]]) 
 
 
     def bestFit(self,blocksCount):
         smallestFreeSpace = self.freeSpaceList.head.getFreeBlocks()
         position = self.freeSpaceList.head.getPosition()
         for nodeFreeSpace in self.freeSpaceList:
-            if (blocksCount <= nodeFreeSpace.getFreeBlocks() <=  smallestFreeSpace):
+            if ((blocksCount <= nodeFreeSpace.getFreeBlocks() <=  smallestFreeSpace) or (smallestFreeSpace<blocksCount)):
                 smallestFreeSpace = nodeFreeSpace.getFreeBlocks()
                 position = nodeFreeSpace.getPosition()
         for nodeFreeSpace in self.freeSpaceList:
             if(nodeFreeSpace.hasTheSamePosition(position)):
                 nodeFreeSpace.setPosition(nodeFreeSpace.getPosition()+blocksCount)
                 nodeFreeSpace.setFreeBlocks(nodeFreeSpace.getFreeBlocks()-blocksCount)
-                if(nodeFreeSpace.getFreeBlocks()==0):
+                if(nodeFreeSpace.getFreeBlocks()<=0):
                     self.freeSpaceList.removeFromFreeSpace(nodeFreeSpace.getPosition())
                 return position
                 
@@ -66,13 +76,27 @@ class HeapMemory:
             if (nodeFreeSpace.hasTheSamePosition(position)):
                 nodeFreeSpace.setPosition(nodeFreeSpace.getPosition()+blocksCount)
                 nodeFreeSpace.setFreeBlocks(nodeFreeSpace.getFreeBlocks()-blocksCount)
-                if (nodeFreeSpace.getFreeBlocks()==0):
+                if (nodeFreeSpace.getFreeBlocks()<=0):
                     self.freeSpaceList.removeFromFreeSpace(nodeFreeSpace.getPosition())
                 print(nodeFreeSpace.getPrintableFreeBlocksList())
                 return position
    
 
 
+    def updateHeap(self):
+        keysListIndex = list(self.index.keys())
+        valuesListIndex = list(self.index.values())
+        keysListIndexQtd = list(self.indexQtd.keys())
+        valuesListIndexQtd = list(self.indexQtd.values())
+    
+        for i in range(len(self.heapArray)):
+            self.heapArray[i] = False
+            self.heapArrayValues[i] = ''
+        for i in range(len(keysListIndex)):
+            for j in range(valuesListIndex[i],valuesListIndex[i]+valuesListIndexQtd[i]):
+                self.heapArray[j] = True
+                self.heapArrayValues[j] += keysListIndexQtd[i]+" "
+    
     def setHeap(self, position, blocks,value):
         for i in range(position, position+blocks):
             self.heapArray[i] = True
@@ -86,6 +110,7 @@ class HeapMemory:
         return countTotal > 1
 
     def removeBlock(self,blcokIndex):
+        #se tem espaço antes e o espaço acaba junto ao bloco que vai ser removido/ liberado
         if( (self.hasSpaceBeforeEnd(blcokIndex)) and (self.hasSpaceBefore(blcokIndex))):
             for nodeFreeSpace  in self.freeSpaceList:
                 if(nodeFreeSpace.getNextNode().getPosition()>blcokIndex):
@@ -150,10 +175,11 @@ class HeapMemory:
 
 
     def showHeap(self):
+        self.updateHeap()
         print('===============SHOWING HEAP======================')
-        print("|position || In Use |")
+        print("| position || In Use || Value |")
         for indexOfHeap,value in enumerate(self.heapArray): 
-            print('|%(index)d || %(inUse)s |' % {"index": indexOfHeap,'inUse':value,'value':self.heapArrayValues[indexOfHeap]})
+            print('| %(index)d || %(inUse)s || %(value)s |' % {"index": indexOfHeap,'inUse':value,'value':self.heapArrayValues[indexOfHeap]})
         print("===========LIST OF FREE SPACE============")
         print(self.freeSpaceList.getPrintableFreeBlocksList())
         print('=========END OF SHOWING HEAP======================')
